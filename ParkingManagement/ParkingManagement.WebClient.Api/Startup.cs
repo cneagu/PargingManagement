c8d32c47f1eaff390a884b90a5cdf997efefabe2
neagu cristi <neagucristianstefan@yahoo.com>
2021-10-13 19:54:41 +0300

    create authentication controller and add nughet pakages

--- ParkingManagement/ParkingManagement.WebClient.Api/Startup.cs
+++ ParkingManagement/ParkingManagement.WebClient.Api/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ParkingManagement.WebClient.Api
 {
@@ -44,11 +37,12 @@ namespace ParkingManagement.WebClient.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParkingManagement.WebClient.Api v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
